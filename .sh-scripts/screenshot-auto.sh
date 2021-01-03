5b30e1efda13fba597c685e9d55fa2e6601d53a4
j0eii <developer@example.com>
2021-01-03 20:49:23 +0800

    fix: capitalize issue

--- .sh-scripts/screenshot-auto.sh
+++ .sh-scripts/screenshot-auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 timestamp="$(date +%Y%m%d%H%M%S)"
-targetbase="$HOME/screenshots"
+targetbase="$HOME/Screenshots"
 mkdir -p $targetbase
 [ -d $targetbase ] || exit 1
-touch $targetbase/$timestamp.txt
-sleep 0.2 && scrot "$targetbase/$timestamp.png" -sb -q 10
+#touch $targetbase/$timestamp.txt
+sleep 0.5 && scrot "$targetbase/$timestamp.png" -sb -q 10
